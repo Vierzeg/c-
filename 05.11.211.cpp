fc3bb8593fe734e10d87b2c535980d13038359da
Vierzeg <slipknotjoey66@gmail.com>
2021-11-05 20:53:45 +0300

    commit1c

---
+++ 05.11.211.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+void addStr(string*& str)
+{
+    int size = 0;
+    string* buf = new string[size + 1];
+    for (int i = 0; i < size; i++) {
+        buf[i] = str[i];
+    }
+    delete[]str;
+    str = buf;
+    size++;
+}
+
+int main()
+{
+    string* array = new string[0];
+}
